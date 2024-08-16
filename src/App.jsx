20e2dd4775394ee59774c3c1cc0f8b73ec22c444
Abhay <abhayd0331@gmail.com>
2024-08-16 20:03:16 +0530

    Error solved

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
   Outlet,
-  Route,
 } from "react-router-dom";
-import RandomUser from "./Components/RandomUser";
-import CatList from "./Components/CatList";
-import RandomJoke from "./Components/RandomJoke";
 
 function App() {
   const [count, setCount] = useState(0);
